9ebbf1ee5e9ada111eac620fb5eefe5fe7a67fdd
Laien Xie <angela@Laiens-MacBook-Air.local>
2020-07-28 16:10:11 -0700

    day 2 - added 3 custom features

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,8 +2,9 @@ import React, {useState, useCallback, useRef} from 'react';
 import produce from 'immer';
 import './App.css';
 
-const numRows = 68;
-const numCols = 68;
+// An appropriate data structure to hold a grid of cells that is at least 25x25
+const numRows = 28;
+const numCols = 28;
 
 const operations = [
   [0, 1],
@@ -16,7 +17,7 @@ const operations = [
   [-1, 0]
 ];
 
-const generateEmptyGrid = () => {
+const emptyGrid = () => {
   const rows = [];
   for (let i = 0; i < numRows; i++) {
     rows.push(Array.from(Array(numCols), () => 0))
@@ -25,46 +26,67 @@ const generateEmptyGrid = () => {
   return rows;
 }
 
+const generateCell = (g) => {
+  return produce(g, gridCopy => {
+    for (let i = 0; i < numRows; i++) {
+      for (let h = 0; h < numCols; h++) {
+        // Examine state of all eight neighbors
+        let neighbors = 0;
+        operations.forEach(([x, y]) => {
+          const newI = i + x;
+          const newH = h + y;
+          // Apply rules of life to determine if this cell will change states
+          if (newI >= 0 && newI < numRows && newH >= 0 && newH < numCols) {
+            neighbors += g[newI][newH]
+          }
+        })
+        if (neighbors < 2 || neighbors > 3) {
+          gridCopy[i][h] = 0;
+        } else if (g[i][h] === 0 && neighbors === 3) {
+          gridCopy[i][h] = 1;
+        }
+      }
+    }
+  });
+}
+
 function App() {
+  // Grid to display cells
   const [grid, setGrid] = useState(() => {
-    return generateEmptyGrid()
+    return emptyGrid()
   });
 
   const[running, setRunning] = useState(false);
 
+  const [speed, setSpeed] = useState(false);
+
   const runningRef = useRef(running);
-  runningRef.current = running
+  runningRef.current = running;
 
   const runSimulation = useCallback(() => {
     if (!runningRef.current) {
       return;
     }
     setGrid((g) => {
-      return produce(g, gridCopy => {
-        for (let i = 0; i < numRows; i++) {
-          for (let k = 0; k < numCols; k++) {
-            let neighbors = 0;
-            operations.forEach(([x, y]) => {
-              const newI = i + x;
-              const newK = k + x;
-              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
-                neighbors += g[newI][newK]
-              }
-            })
-            if (neighbors < 2 || neighbors > 3) {
-              gridCopy[i][k] = 0;
-            } else if (g[i][k] === 0 && neighbors === 3) {
-              gridCopy[i][k] = 1;
-            }
-          }
-        }
-      });
+      return generateCell(g);
     });    
-    setTimeout(runSimulation, 100);
-  }, [])
+    // Utilize a timeout function to build the next generation of cells
+    setTimeout(runSimulation, speed ? 100 : 1000);
+  }, [speed]);
+
+  const generationStep = useCallback(() => {
+    if (!runningRef.current) {
+      return;
+    }
+    setGrid((g) => {
+      return generateCell(g);
+    });
+  }, []);
 
   return (
     <>
+      <h1>Game of Life</h1>
+      {/* Button(s) that start & stop the animation */}
       <button
         onClick={() => {
           setRunning(!running);
@@ -74,13 +96,18 @@ function App() {
           }
         }}
       >
-        {running ? 'stop' : 'start'}
+        {running ? 'Stop' : 'Start'}
       </button>
-      <button onClick={() => {
-        setGrid(generateEmptyGrid());
-      }}>
-        Clear
+      {/* Provide functionality to manually step through the simulation one generation at a time, as opposed to animating automatically */}
+      <button
+        onClick={() => {
+          runningRef.current = true;
+          generationStep();
+        }}
+      >
+        Cell by Generation
       </button>
+      {/* CUSTOM FEATURES #1 Add an option that creates a random cell configuration that users can run */}
       <button onClick={() => {
         const rows = [];
         for (let i = 0; i < numRows; i++) {
@@ -92,25 +119,53 @@ function App() {
       }}>
         Random
       </button>
+      {/* CUSTOM FEATURES #2 Allow users to specify the speed of the simulation */}
+      {!speed ? (
+          <button
+            onClick={() => {
+              setSpeed(true);
+            }}
+          > 
+            10x Speed 
+          </button>
+        ) : (
+          <button
+            onClick={() => {
+              setSpeed(false);
+            }}
+          >
+            1x Speed 
+          </button>
+        )}
+      {/* Button to clear the grid */}
+      <button onClick={() => {
+        setGrid(emptyGrid());
+      }}>
+        Clear
+      </button>
+      {/* Cell objects or components properties*/}
       <div style={{
         display: 'grid',
-        gridTemplateColumns: `repeat(${numCols}, 10px)`
+        gridTemplateColumns: `repeat(${numCols}, 15px)`
       }}>
         {grid.map((rows, i) => 
-          rows.map((col, k) => 
+          rows.map((col, h) => 
           <div 
-          key={`${i}-${k}`}
+          key={`${i}-${h}`}
+          // Clickable/Tappable
           onClick={() => {
+            // can be clicked to allow user to setup initial cell configuration
             const newGrid = produce(grid, gridCopy => {
-              gridCopy[i][k] = grid[i][k] ? 0 : 1;
-
+              // Toggle state functionality: switch between alive & dead either because user manually toggled cell before starting simulation
+              gridCopy[i][h] = grid[i][h] ? 0 : 1;
             })
             setGrid(newGrid);
           }}
             style={{
-              width: 10,
-              height: 10, 
-              backgroundColor: grid[i][k] ? 'pink' : undefined,
+              width: 15,
+              height: 15, 
+              // current state: (alive, dead), (green, white)
+              backgroundColor: grid[i][h] ? 'mediumspringgreen' : undefined,
               border: "solid 1px black"
             }}
           />
