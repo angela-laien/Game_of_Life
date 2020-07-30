ff354750bb7504d36fb31e319c38e56f77b3d6e4
Laien Xie <angela@Laiens-MacBook-Air.local>
2020-07-30 15:27:49 -0700

    Updates

---
+++ precache-manifest.73c740dba7f9d75943dab1391803d434.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0b4652686d7b16846d6b5d3f23bbc77",
+    "url": "/%3Cgithub-repo-name%3E/index.html"
+  },
+  {
+    "revision": "76d9ab38dc18ee1c3de6",
+    "url": "/%3Cgithub-repo-name%3E/static/css/main.a9eca0f2.chunk.css"
+  },
+  {
+    "revision": "7e8bd003990e05f48736",
+    "url": "/%3Cgithub-repo-name%3E/static/js/2.d4508565.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/%3Cgithub-repo-name%3E/static/js/2.d4508565.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76d9ab38dc18ee1c3de6",
+    "url": "/%3Cgithub-repo-name%3E/static/js/main.f47e661d.chunk.js"
+  },
+  {
+    "revision": "79fe531584afe46bd548",
+    "url": "/%3Cgithub-repo-name%3E/static/js/runtime-main.dd1fad1f.js"
+  }
+]);
